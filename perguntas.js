5b3d0c35954104af0508c6888fb1638eba0358da
Yukarimp <168309748+Yukarimp@users.noreply.github.com>
2024-11-25 12:24:43 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'MONTE EVERESTE'
 )
 
 criaCartao(
