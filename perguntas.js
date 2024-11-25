06f98bebc458cb55dc591479adb1afb091d65c6f
Yukarimp <168309748+Yukarimp@users.noreply.github.com>
2024-11-25 12:30:32 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -5,9 +5,9 @@ criaCartao(
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Ciência',
+    'Quantos ossos tem no corpo humano?',
+    '206'
 )
 
 criaCartao(
